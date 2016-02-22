a2c65651d29a2a96f453f8e78454b83a6141adb8
George Gianacopoulos <g.p.gianacopoulos@gmail.com>
2016-02-22 03:43:30 -0500

    Added more drawables, layout files. Replaced SortActivity + created a new Activity.

---
+++ app/src/main/java/io/github/pxsort/activity/FilterManagerActivity.java
@@ -0,0 +1,15 @@
+package io.github.pxsort.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.github.pxsort.R;
+
+public class FilterManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter_manager);
+    }
+}
---
+++ app/src/main/java/io/github/pxsort/activity/SortActivity.java
@@ -0,0 +1,18 @@
+package io.github.pxsort.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Activity for pixel sorting.
+ *
+ * Created by George on 2016-02-16.
+ */
+public class SortActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
